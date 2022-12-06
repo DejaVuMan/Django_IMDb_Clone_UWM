e6d2a9d1d534dc4a5881c8aed4564015abaefe17
Daniel Młynarczyk <d.mlynarczyk16@gmail.com>
2022-12-07 00:12:44 +0100

    create users list model

--- project/imdb/models.py
+++ project/imdb/models.py
@@ -1,6 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserMoviesList(models.Model):
+
+        ACTION = 'Action'
+        ADVENTURE = 'Adventure'
+        ANIMATED = 'Animated'
+        COMEDY = 'Comedy'
+        DRAMA = 'Drama'
+        FANTASY = 'Fantasy'
+        HISTORICAL = 'Historical'
+        HORROR = 'Horror'
+        MUSICAL = 'Musical'
+        ROMANCE = 'Romance'
+        SCIENCE_FICTION = 'Science Fiction'
+        THRILLER = 'Thriller'
+        WESTERN = 'Western'
+        OTHER = 'Other'
+
+        TYPE_OF_LIST = [
+            (ACTION, 'Action'),
+            (ADVENTURE, 'Adventure'),
+            (ANIMATED, 'Animated'),
+            (COMEDY, 'Comedy'),
+            (DRAMA, 'Drama'),
+            (FANTASY, 'Fantasy'),
+            (HISTORICAL, 'Historical'),
+            (HORROR, 'Horror'),
+            (MUSICAL, 'Musical'),
+            (ROMANCE, 'Romance'),
+            (SCIENCE_FICTION, 'Science Fiction'),
+            (THRILLER, 'Thriller'),
+            (WESTERN, 'Western'),
+            (OTHER, 'Other'),
+        ]
+
+        title = models.CharField(max_length=50)
+        description = models.CharField(max_length=255)
+        type = models.CharField(
+            max_length=15,
+            choices=TYPE_OF_LIST,
+            default=OTHER
+    )
+        is_public = models.BooleanField(default=True)
+
+
 class User(models.Model):
 
     class Roles(models.IntegerChoices):
@@ -12,6 +58,9 @@ class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=20)
     role = models.IntegerField(choices=Roles.choices, default=Roles.USER)
+    film_lists = models.ForeignKey(UserMoviesList, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+
