90af425f88c8dbadff0d8bef9db98cb623d455ed
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-12-06 20:23:15 +0100

    Implemented hotfix for security purposes, imported decouple for use of .env file to hold secure secrets

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*=#4n&tk*6$ky!l*f-=skyq6u*+w!m!_0k(*jsmxuxjlm)1893'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,7 +79,7 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': config("PGSQL_PASSWORD"),
         'NAME': 'imdb',
         'HOST': 'localhost',
         'PORT': 5432,
