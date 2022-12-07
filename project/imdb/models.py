c0bed8674052e02ac454098181fc726ceb30cff3
DejaVuMan <58810486+DejaVuMan@users.noreply.github.com>
2022-12-08 00:05:51 +0100

    Implemented People and Movie model, added ManyToManyField for UserMoviesList

--- project/imdb/models.py
+++ project/imdb/models.py
@@ -1,15 +1,71 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
+class People(models.Model):
+    class Meta:
+        verbose_name = 'Person'
+        verbose_name_plural = 'People'
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    date_of_birth = models.DateField()
+    birth_location = models.CharField(max_length=100)
+    bio = models.TextField()
+
+
+class Movie(models.Model):
+    class Meta:
+        verbose_name = 'Movie'
+        verbose_name_plural = 'Movies'
+
+    ACTION = 'Action'
+    ADVENTURE = 'Adventure'
+    ANIMATED = 'Animated'
+    COMEDY = 'Comedy'
+    DRAMA = 'Drama'
+    FANTASY = 'Fantasy'
+    HISTORICAL = 'Historical'
+    HORROR = 'Horror'
+    MUSICAL = 'Musical'
+    ROMANCE = 'Romance'
+    SCIENCE_FICTION = 'Science Fiction'
+    THRILLER = 'Thriller'
+    WESTERN = 'Western'
+    OTHER = 'Other'
+
+    TYPE_OF_LIST = [
+        (ACTION, 'Action'),
+        (ADVENTURE, 'Adventure'),
+        (ANIMATED, 'Animated'),
+        (COMEDY, 'Comedy'),
+        (DRAMA, 'Drama'),
+        (FANTASY, 'Fantasy'),
+        (HISTORICAL, 'Historical'),
+        (HORROR, 'Horror'),
+        (MUSICAL, 'Musical'),
+        (ROMANCE, 'Romance'),
+        (SCIENCE_FICTION, 'Science Fiction'),
+        (THRILLER, 'Thriller'),
+        (WESTERN, 'Western'),
+        (OTHER, 'Other'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    year = models.IntegerField()
+    director = models.ForeignKey(People, on_delete=models.CASCADE, related_name='director')
+    genre = models.CharField(max_length=100, choices=TYPE_OF_LIST, default=OTHER)
+    rating = models.FloatField(default=0.0)
 
 
 class UserMoviesList(models.Model):
-
     class Meta:
         verbose_name = 'User Film List'
         verbose_name_plural = 'User Film Lists'
 
+    # TODO: It might get repetitive to have to keep on adding these lists to models, look into mixin viability
     ACTION = 'Action'
     ADVENTURE = 'Adventure'
     ANIMATED = 'Animated'
@@ -44,28 +100,23 @@ class UserMoviesList(models.Model):
 
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
-    type = models.CharField(
+    genre = models.CharField(
         max_length=15,
         choices=TYPE_OF_LIST,
         default=OTHER
     )
 
+    movies = models.ManyToManyField(Movie, related_name='movies', blank=True)
+
     is_public = models.BooleanField(default=True)
 
 
-#class UserMoviesListItem(models.Model):
-    #movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    #list = models.ForeignKey(UserMoviesList, on_delete=models.CASCADE)
-# TODO: Implement Movie model before implementing UserMoviesListItem as UMLI will have dependency on Movie model
-
-
 class User(AbstractUser):
-
     class Roles(models.IntegerChoices):
         USER = 0
         ADMIN = 1
 
-    #default fields from User are already passed here
+    # default fields from User are already passed here
     first_name = models.CharField(max_length=33)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
