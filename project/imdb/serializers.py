a6e43dc8fc9423cea887ec1d02aea281a6d5ff19
Daniel <d.mlynarczyk16@gmail.com>
2022-12-08 16:50:52 +0100

    add class Meta for serializers

--- project/imdb/serializers.py
+++ project/imdb/serializers.py
@@ -3,21 +3,25 @@ from .models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
-    model = User
-    fields = "__all__"
+   class Meta:
+       model = User
+       fields = "__all__"
 
 
 class PeopleSerializer(serializers.ModelSerializer):
-    model = People
-    fields = ['name', 'age', 'date_of_birth', 'birth_location', 'bio']
+    class Meta:
+        model = People
+        fields = ['name', 'age', 'date_of_birth', 'birth_location', 'bio']
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    model = Movie
-    fields = '__all__'
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 class UserMoviesListSerializer(serializers.ModelSerializer):
-    model = UserMoviesList
-    fields = '__all__'
+    class Meta:
+        model = UserMoviesList
+        fields = '__all__'
 
