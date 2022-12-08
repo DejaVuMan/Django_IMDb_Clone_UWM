11fd15e99ac8c84dbbe69c45af932e222ae25748
Daniel <d.mlynarczyk16@gmail.com>
2022-12-08 16:36:56 +0100

    add endpoint for display users

--- project/imdb/views.py
+++ project/imdb/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
+from django.core.exceptions import PermissionDenied
 
 
-def index(request):
-    return HttpResponse("/api works")
+@api_view(['GET'])
+def users_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
