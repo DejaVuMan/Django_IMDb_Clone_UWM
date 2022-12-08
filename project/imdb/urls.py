f58e7c0b931011fd87e530e099a1b39d956960f7
DejaVuMan <szuszkiewiczalan928@gmail.com>
2022-12-08 17:21:46 +0100

    Began working on views and urls

--- project/imdb/urls.py
+++ project/imdb/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('users', views.users_list),
+    path('users/<int:pk>', views.user_detail),
+    path('users/create', views.user_create),
+    path('users/<int:pk>/update', views.user_update),
+    path('users/<int:pk>/delete', views.user_delete),
+    path('users/<int:pk>/movies', views.user_movies_list),
+    path('movies', views.movies_list),
+    path('movies/<query>', views.movies_search),
+    path('lists', views.all_user_movies_list),
 ]
